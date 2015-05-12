20098b0a09a6bfb71cd8c01fa3a61aa978f8ae8e
Alan Wright <alanwright@Alans-MacBook-Pro.local>
2015-05-12 14:28:33 +1000

    Styling minor functionality

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-$(document).ready(function() {
+ var clearBoard = function() {
+   $('.container').children().removeClass('naught cross');
+ };
 
-
-
-  $('.box').on('click', function() {
-
-    if( ($(this).hasClass("cross")) || ($(this).hasClass("naught")) ){
-      return;
+var noMoreMoves = function(){
+    var number = $('.naught').length + $('.cross').length;
+    if(number === 9){
+      return alert('Game Over Man!')
     }
 
-    var player = $("input:radio:checked").val();
-    if (player === "playerX") {
-      $(this).addClass("cross");
-      $("#playerO").prop("checked", true);
-    } else {
-      $(this).addClass("naught");
-      $("#playerX").prop("checked", true);
+}
 
-    }
+ $(document).ready(function() {
 
-  });
+   $('.box').on('click', function() {
+
+     if (($(this).hasClass("cross")) || ($(this).hasClass("naught"))) {
+       return;
+     }
+
+     var player = $("input:radio:checked").val();
+     if (player === "playerX") {
+       $(this).addClass("cross");
+       $("#playerO").prop("checked", true);
+     } else {
+       $(this).addClass("naught");
+       $("#playerX").prop("checked", true);
+     }
+     noMoreMoves();
+   });
+
+   $('.clearBoard').on('click', clearBoard);
+
+   
+
+   
 
 
 
-
-
-
-});
+ });
